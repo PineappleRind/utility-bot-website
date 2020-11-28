200f4210352bb290527c2fbc3a760ada5519caf1
PineappleRind <pineapplerind.info@gmail.com>
2020-11-28 17:04:17 -0500

    Gaussian blur when nav open, underline transition

--- src/frontend/js/app.dev.js
+++ src/frontend/js/app.dev.js
@@ -3,6 +3,7 @@
 "use strict";
 var t = document.getElementById("menuOpen"),
     r = document.getElementById("navShade"),
+    c = document.getElementById("blurOverlay"),
     s = {
         n: document.querySelector(".nav-toggle"),
         l: function(e) {
@@ -10,13 +11,23 @@ var t = document.getElementById("menuOpen"),
         }
     };
 
+
 function openNav() {
-    "300px" === t.style.marginRight ? (t.style.marginRight = "0px", r.style.left = "100%") : (t.style.marginRight = "300px", setTimeout(function() {
-        r.style.left = "calc(100% - 400px)"
-    }, 300))
+   if (r.style.left === "calc(100% - 300px)") {
+       r.style.left = "100%";
+       setTimeout(function(){c.style.display = 'none';
+   },500);
+       c.style.opacity = '0';
+   } else {
+       r.style.left = "calc(100% - 300px)";
+       c.style.display = 'block';
+       setTimeout(function(){c.style.opacity = '1';
+    },5);
+   }
 }
 t.addEventListener("click", function(e) {
-    openNav(), s.l(e)
+    openNav();
+    s.l(e);
 });
 
 console.log("JavaScript by Aditya (twitter.com/TheCodingGuru) and PineappleRind (https://pineapplerind.github.io), 2020")
