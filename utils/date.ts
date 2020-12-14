325350aa6c68a66d12817ee69ac762f6017fea1d
andrew <andrewnijmeh1@gmail.com>
2020-12-14 16:02:29 -0500

    add CPU/server stats types + server/shard types for shard info + server discovery"

--- utils/date.ts
+++
@@ -1,6 +0,0 @@
-// We can use this time to show the time certain shard's of Utility Bot e.g UTC, EST etc. this is to format time ;)
-
-import {format} from 'date-fns'
-
-const format Date = (date: Date | string) =>
-    formate(typeof date ==='string' ? new Date(date) ; Date, 'do MMMM, yyyy')
---
+++ utils/types.ts
@@ -0,0 +1,28 @@
+
+export interface Server {
+    name: string;
+    id: string;
+    icon: string;
+    vanity: string;
+    members: number;
+
+}
+
+
+export interface Shard{
+    id: number;
+    guilds: number;
+    unavaibleGuilds: number;
+    users: number;
+    
+}
+export interface Stats{
+    cpu: number;
+    ram: string;
+    totalRam: string;
+    shardCount: number;
+    guilds: number;
+    users: number;
+
+}
+
