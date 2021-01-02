b2b85b3bd6665e2d450eb3a48009bb425b92d251
titong0 <augustoerrecarte@gmail.com>
2021-01-01 22:52:36 -0300

    added cards
    just specify the title in the props of the card component and the content as the children

--- utility-bot-website/src/Components/Home.js
+++ utility-bot-website/src/Components/Home.js
@@ -1,8 +1,8 @@
 import RippleButton from "./RippleButton";
-
+import Card from "./card";
 const Home = () => {
   return (
-    <div>
+    <div style={{ backgroundColor: "var(--grey)" }}>
       <div className="section-container">
         <div>
           <h1 className="section-content">
@@ -11,6 +11,16 @@ const Home = () => {
           </h1>
           <RippleButton content="invite" />
         </div>
+      </div>
+      <div className="cards">
+        <Card title="title">
+          content of the cardcontent of the cardcontent of the cardcontent of
+          the cardcontent of the cardcontent of the cardcontent of the
+          cardcontent of the cardcontent of the cardcontent of the cardcontent
+          of the cardcontent of the card
+        </Card>
+        <Card title="title">content</Card>
+        <Card title="title">content of the card</Card>
       </div>
     </div>
   );
